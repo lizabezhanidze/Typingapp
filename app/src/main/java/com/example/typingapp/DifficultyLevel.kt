034d6c019e1493693717c3e9e7253e8fe9416c45
BAG0N <alpenid2004@gmail.com>
2025-07-09 22:08:12 +0400

    remove line

--- app/src/main/java/com/example/typingapp/DifficultyLevel.kt
+++ app/src/main/java/com/example/typingapp/DifficultyLevel.kt
@@ -7,7 +7,6 @@ enum class DifficultyLevel(val value: Int) {
 
     companion object {
         fun fromInt(value: Int) = values().firstOrNull { it.value == value } ?: EASY
-
         fun fromString(name: String) = values().firstOrNull { it.name.equals(name, ignoreCase = true) } ?: EASY
     }
 }
